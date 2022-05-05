3ff80d7e2e0023949caa52ffafaa6b1b8b475c74
DaruZero <matteo@danelon.me>
2022-05-05 16:15:29 +0200

    Added C# solution for 1603. Design Parking System

---
+++ 1603. Design Parking System/C#/Design Parking System/ParkingSystem.cs
@@ -0,0 +1,32 @@
+﻿namespace Design_Parking_System;
+
+public class ParkingSystem
+{
+    public ParkingSystem(int big, int medium, int small)
+    {
+        BigSlots = big;
+        MediumSlots = medium;
+        SmallSlots = small;
+    }
+
+    public int BigSlots { get; set; }
+    public int MediumSlots { get; set; }
+    public int SmallSlots { get; set; }
+
+    public bool AddCar(int carType)
+    {
+        switch (carType)
+        {
+            case 1:
+                BigSlots--;
+                return BigSlots >= 0;
+            case 2:
+                MediumSlots--;
+                return MediumSlots >= 0;
+            case 3:
+                SmallSlots--;
+                return SmallSlots >= 0;
+            default: return false;
+        }
+    }
+}
---
+++ 1603. Design Parking System/C#/Design Parking System/Program.cs
@@ -0,0 +1,33 @@
+﻿using Design_Parking_System;
+
+int bigSlots, mediumSlots, smallSlots;
+
+Console.WriteLine("Insert number of big slots:");
+while (!int.TryParse(Console.ReadLine(), out bigSlots))
+    Console.WriteLine("Insert a valid number!");
+
+Console.WriteLine("Insert number of medium slots:");
+while (!int.TryParse(Console.ReadLine(), out mediumSlots))
+    Console.WriteLine("Insert a valid number!");
+
+Console.WriteLine("Insert number of small slots:");
+while (!int.TryParse(Console.ReadLine(), out smallSlots))
+    Console.WriteLine("Insert a valid number!");
+
+Console.WriteLine("Park one car of type:");
+Console.WriteLine("\t1 Big car");
+Console.WriteLine("\t2 Medium car");
+Console.WriteLine("\t3 Small car");
+
+ParkingSystem parkingSystem = new(bigSlots, mediumSlots, smallSlots);
+
+// Iterate enough time to fill all the parking spots
+var i = bigSlots + mediumSlots + smallSlots;
+for (; i > 0; i--)
+{
+    var key = Console.ReadKey().KeyChar;
+    Console.WriteLine(key is '1' or '2' or '3'
+        ? $"\nCan park car? {parkingSystem.AddCar(int.Parse(key.ToString()))}"
+        : "Insert a valid number!");
+    Console.WriteLine("Park another car:");
+}
