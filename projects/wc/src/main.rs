a4b26c61306fedf8d9a8d8f560dd6788fd5b467d
Matteo Danelon <matteo@danelon.me>
2024-09-15 17:01:05 +0200

    Implement count_bytes

--- projects/wc/src/main.rs
+++ projects/wc/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env::args, process::exit};
+use std::{env::args, fs::File, io::Read, path::Path, process::exit};
 
 fn main() {
     let args: Vec<_> = args().collect();
@@ -16,7 +16,7 @@ fn main() {
     }
 
     if args[1] == "-c" || args[1] == "--bytes" {
-        count_bytes()
+        let bytes = count_bytes(args[2].as_str());
     }
 }
 
@@ -30,6 +30,19 @@ The options below may be used
     );
 }
 
-fn count_bytes() {
-    panic!("not implemented");
+fn count_bytes(path_to_file: &str) -> usize {
+    let path = Path::new(path_to_file);
+    let display = path.display();
+
+    let mut file = match File::open(path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut buf = Vec::new();
+    if let Err(why) = file.read_to_end(&mut buf) {
+        panic!("couldn't read {}: {}", display, why)
+    }
+
+    buf.len()
 }
